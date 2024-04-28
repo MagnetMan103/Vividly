537b364248db45878f10d79302bc0dca9abf6f01
Alan Munschy <alanmunschy@gmail.com>
2024-04-28 03:49:35 -0400

    Connected GPT to front end. Created text to speech for GPT text. Finished story scene.

--- screens/EventScreen.jsx
+++ screens/EventScreen.jsx
@@ -1,28 +1,71 @@
-import {Text, View, Image, FlatList} from "react-native";
+import {Text, View, Image, FlatList, Pressable} from "react-native";
 import {useEffect} from "react";
 import {useState} from "react";
+import {useNavigation} from "@react-navigation/native";
 export default function EventScreen({route}) {
     const [event, setEvent] = useState('')
     const [images, setImages] = useState([])
-
+    const navigation = useNavigation()
     useEffect(() => {
         fetch(`http://34.145.192.60/api/events/${route.params.id}/`)
             .then((response) => response.json())
             .then((json) => {
                 setEvent(json);
-                setImages(json.images.split(" "))
+                if (json.images !== "") {
+                    setImages(json.images.split(" "))
+                }
                 // setImages(event.images.split(" "))
             })
             .catch(error => console.error(error));
 
     }, []);
+    const deleteMemory = () => {
+        fetch(`http://34.145.192.60/api/events/${event.id}/`, {
+            method: 'DELETE',
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Failed to delete memory');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log('Memory deleted successfully:', data);
+                navigation.navigate('Home')
+                // Optionally, you can perform further actions after deletion
+            })
+            .catch(error => {
+                console.error('Error deleting memory:', error);
+            });
+    }
+    if (images === []) {
+        alert('no images')
+        return (
+            <View style={{flex: 1}}>
+                <Text style={{fontSize: 30, fontWeight: 'bold'}}>{event.title}</Text>
+                <Text style={{fontSize: 20}}>{event.time}</Text>
+                <Text style={{fontSize: 30}}>{event.description}</Text>
+                <Pressable style={{borderWidth: 3, borderColor: 'gray', backgroundColor: 'red'}}
+                onPress={deleteMemory}>
+                    <Text style={{fontSize: 30, fontWeight: "bold"}}>
+                        Delete Memory</Text>
+                </Pressable>
+            </View>
+        )
+    }
     return (
         <View style={{flex: 1}}>
             <Text style={{fontSize: 30, fontWeight: 'bold'}}>{event.title}</Text>
             <Text style={{fontSize: 20}}>{event.time}</Text>
             <Text style={{fontSize: 30}}>{event.description}</Text>
             <FlatList vertical={true}
-                      data={images} renderItem={({item}) => <Image source={{ uri: item }} style={{ width: 150, height: 150 }}/>}/>
+                      data={images} renderItem={({item}) =>
+                <Image source={{ uri: item }} style={{ width: 150, height: 150 }}/>}/>
+            <Pressable style={{borderWidth: 3, borderColor: 'gray', backgroundColor: 'red'}}
+            onPress={deleteMemory}>
+                <Text style={{fontSize: 30, fontWeight: "bold"}}>
+                    Delete Memory</Text>
+            </Pressable>
 
 
 
--- screens/GPTScreen.jsx
+++ screens/GPTScreen.jsx
@@ -1,8 +1,89 @@
-import {View, Text} from "react-native";
+import {View, Text, Pressable} from "react-native";
+import {useState} from "react";
+import {useEffect} from "react";
+import {ScrollView} from "react-native";
+import * as Speech from 'expo-speech';
+
 export default function GPTScreen() {
+    const [responsed, setResponse] = useState("Response from GPT will appear here.")
+    const [user_input, setUserInput] = useState("")
+    const [isPressable, setIsPressable] = useState(false)
+    const [opacit, setOpacit] = useState(1)
+    const [opacitw, setOpacitw] = useState(0)
+    const system_input = "You are designed to generate a short 'story of my life' from " +
+        "given information for alzheimer's patients. Give a creative story told in 2nd person. " +
+        "Use months instead of dates."
+    useEffect(() => {
+        Speech.speak('Hello World!')
+        fetch('http://34.145.192.60/api/events/')
+            .then((response) => response.json())
+            .then((json) => {
+                setUserInput(createGPTInput(json));
+                console.log(user_input)
+                console.log('tried')
+            })
+            .catch(error => console.error(error));
+
+    }, []);
+    const readGPT = () => {
+        Speech.speak(responsed)
+    }
+    const callGPT = () => {
+        console.log('will send')
+        console.log(user_input)
+        if (user_input === "") {
+            alert('Please try again')
+            return
+        }
+        setIsPressable(true)
+        setOpacit(0)
+        setOpacitw(1)
+        const GPTData = {
+            system_input: system_input,
+            user_input: user_input,
+
+        };
+        fetch('http://34.145.192.60/api/ai/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(GPTData)
+        })
+            .then((response) => response.json())
+            .then((json) => {
+                console.log(json)
+                setResponse(json.result)
+            })
+            .catch(error => console.error(error));
+    }
     return (
         <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <Text style={{fontSize: 30}}>GPT Screen</Text>
+            <Pressable onPress={callGPT} disabled={isPressable}
+            style={{backgroundColor:'gray', opacity:opacit}}>
+                <Text>Generate Story</Text>
+            </Pressable>
+            <Pressable onPress={readGPT} disabled={!isPressable}
+                       style={{backgroundColor:'gray', opacity:opacitw}}>
+                <Text>Read Text</Text>
+            </Pressable>
+            <ScrollView>
+                <Text style={{fontSize: 30, fontWeight: 'bold'}}>Your Story</Text>
+                <Text style={{fontSize: 20}}>{responsed}</Text>
+            </ScrollView>
+
         </View>
-    );
+        )
+}
+
+function createGPTInput(data) {
+    let finalString = ""
+    let number = 1
+    data.events.forEach(event => {
+        finalString += `Date:${event.time} Title:${event.title} Description:${event.description}. `
+        number++
+        console.log(number)
+    })
+    console.log(finalString)
+    return finalString
 }
